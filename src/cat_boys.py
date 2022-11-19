1d998ef480e0f7b6b12cdc8459e7f3842dd3e900
ecclesias <77536370+zeviel@users.noreply.github.com>
2022-11-19 20:28:55 +0800

    Update cat_boys.py

--- src/cat_boys.py
+++ src/cat_boys.py
@@ -2,37 +2,43 @@ from requests import get
 
 
 class CatBoys:
-	def __init__(self):
+	def __init__(self) -> None:
 		self.api = "https://api.catboys.com"
 		self.headers = {
 			"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.99 Safari/537.36"
 		}
 	
-	def ping(self):
+	def ping(self) -> dict:
 		return get(
-			f"{self.api}/ping", headers=self.headers).json()
+			f"{self.api}/ping",
+			headers=self.headers).json()
 	
-	def get_endpoints(self):
+	def get_endpoints(self) -> dict:
 		return get(
 			f"{self.api}/endpoints",
 			headers=self.headers).json()
 	
-	def get_random_image(self):
+	def get_random_image(self) -> dict:
 		return get(
-			f"{self.api}/img", headers=self.headers).json()
+			f"{self.api}/img",
+			headers=self.headers).json()
 	
-	def get_random_baka_gif(self):
+	def get_random_baka_gif(self) -> dict:
 		return get(
-			f"{self.api}/baka", headers=self.headers).json()
+			f"{self.api}/baka",
+			headers=self.headers).json()
 	
-	def get_random_8ball_saying(self):
+	def get_random_8ball_saying(self) -> dict:
 		return get(
-			f"{self.api}/8ball", headers=self.headers).json()
+			f"{self.api}/8ball",
+			headers=self.headers).json()
 	
-	def get_random_number(self):
+	def get_random_number(self) -> dict:
 		return get(
-			f"{self.api}/dice", headers=self.headers).json()
+			f"{self.api}/dice",
+			headers=self.headers).json()
 	
-	def get_random_cat_boy_saying(self):
+	def get_random_cat_boy_saying(self) -> dict:
 		return get(
-			f"{self.api}/catboy", headers=self.headers).json()
+			f"{self.api}/catboy",
+			headers=self.headers).json()
